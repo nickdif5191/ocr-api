e7a6645048f5b2d962f1c8fba0ed8783740d628c
nickdif5191 <njdifiore@gmail.com>
2024-05-28 12:52:40 -0400

    subscription_entities.subscription_entity.py parent class created. subscription_entities.notification_processor and subscription_entities.notification_filter working properly. subscription_entities.attachment_extractor still has a bug.

--- subscription_entities/subscription_entity.py
+++ subscription_entities/subscription_entity.py
@@ -5,10 +5,17 @@ import json
 
 class SubscriptionEntity:
     
-    def __init__(self, input_subscription_name:str):
+    def __init__(self, input_subscription_name:str, output_topic_name: str = None):
+        """
+        Parameters:
+            input_subscription_name (str): name of the subscription this entity pulls from
+            output_topic_name (str, optional): name of topic this entity will publish to
+        """
         self.subscriber = pubsub_v1.SubscriberClient()
         self.publisher = pubsub_v1.PublisherClient()
         self.input_subscription_path = self.subscriber.subscription_path(project=config.project_id, subscription=input_subscription_name)
+        if output_topic_name:
+            self.output_subscription_path = self.publisher.topic_path(project=config.project_id, topic=output_topic_name)
         self.streaming_pull_future = None
         self.name = self.__class__.__name__
     
@@ -21,18 +28,17 @@ class SubscriptionEntity:
         with self.subscriber:
             # Blocks rest of code from running so our pull request is continuously active
             self.streaming_pull_future.result()
-
-    def shutdown(self):
-        if self.streaming_pull_future:
-            self.streaming_pull_future.cancel()  # Gracefully stop the pull
-            self.streaming_pull_future.result()  # Wait for the cancellation to complete
-            print(f"{self.name} pull operation stopped.")
+    
+    def basic_callback(self, message: pubsub_v1.subscriber.message.Message) -> None:
+        print(f"{self.name}: Received {message}.")
+        message.ack()
 
     def callback(self, message:pubsub_v1.subscriber.message.Message):
-        deserialized_message = self.deserialize_incoming_message()
-        self.processing_task(deserialized_message=deserialized_message)
+        deserialized_message = self.deserialize_incoming_message(message=message)
+        processed_message = self.processing_task(deserialized_message=deserialized_message)
+        self.publish_messages(msg_to_publish=processed_message)
         message.ack()
-    
+
     def deserialize_incoming_message(self, message:pubsub_v1.subscriber.message.Message):
         # Decode message from subscription
         decoded_message = message.data.decode('utf-8')
@@ -44,5 +50,23 @@ class SubscriptionEntity:
         Executes processing task that is specific to the entity
         """
         pass
+
+    def publish_messages(self, msg_to_publish):
+        # Encode message to be published
+        json_data = json.dumps(msg_to_publish)
+        encoded_data = json_data.encode('utf-8')
+        # Publish message
+        future = self.publisher.publish(topic=self.output_subscription_path, data=encoded_data)
+        print(f"{self.name} published message ID: {future.result()}\n")
+
+    def shutdown(self):
+        if self.streaming_pull_future:
+            self.streaming_pull_future.cancel()  # Gracefully stop the pull
+            self.streaming_pull_future.result()  # Wait for the cancellation to complete
+            print(f"{self.name} pull operation stopped.")
+
+    
+    
+    
     
         
